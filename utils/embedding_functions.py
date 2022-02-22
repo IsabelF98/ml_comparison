0050a8b9728664b4004d457a9658c160cc4a9d28
Isabel Fernandez <izyf98@gmail.com>
2022-02-22 16:02:13 -0500

    cleaned up function

--- utils/embedding_functions.py
+++ utils/embedding_functions.py
@@ -11,8 +11,8 @@ from scipy.spatial.distance import correlation, cosine, euclidean
 import pandas as pd
 import numpy as np
 
-# Compute Laplacian Eigenmap Embedding
-# ------------------------------------
+# Laplacian Eigenmap Function
+# ---------------------------
 def Laplacain_Eigenmap(data_df,k,n,metric):
     """
     This function computes the lower dimensional embedding of data using the Laplacian Eigemap algorithum.
@@ -24,20 +24,21 @@ def Laplacain_Eigenmap(data_df,k,n,metric):
     
     INPUTS
     ------
-    data_df: data which you are dimensionally reducing
-    k: k value for k-Nearest Neighboor affinity matrix
-    n: number of dimensions you are reducing your data to
-    metric: distance metric you are using to construct affinity matrix (correlation, cosine, euclidean)
+    data_df: (pd.DataFrame) data which you are dimensionally reducing
+    k: (int) k value for k-Nearest Neighboor affinity matrix
+    n: (int) number of dimensions you are reducing your data to
+    metric: (str) distance metric you are using to construct affinity matrix (correlation, cosine, euclidean)
     
     OUTPUTS
     -------
-    LE_data: dimensionaly reduced data (both original and normalized)
+    LE_data: (pd.DataFrame) dimensionaly reduced data (both original and normalized)
              Columns are labeld as dimesnsion number X. Normalized dimensions are label as 'X_norm'.
     """
+    
     # Compute Embedding
     seed             = np.random.RandomState(seed=3) # Initialization seed
     embedding        = SpectralEmbedding(n_components=n, affinity='precomputed', n_jobs=32, random_state=seed) # Transformation
-    X_affinity       = kneighbors_graph(data_df, n_neighbors=k, include_self=True, metric=correlation, n_jobs=52) # Affinity matrix
+    X_affinity       = kneighbors_graph(data_df, n_neighbors=k, include_self=True, metric=metric, n_jobs=52) # Affinity matrix
     X_affinity       = 0.5 * (X_affinity + X_affinity.T) # Normalized affinity
     data_transformed = embedding.fit_transform(X_affinity.toarray()) # Transform data
 
