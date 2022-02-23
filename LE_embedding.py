64e18986c19e3610b6e55c8d1ced79217fa41e92
Isabel Fernandez <izyf98@gmail.com>
2022-02-23 16:45:02 -0500

    changed file to compute embedding seperate form SWC matrix

--- LE_embedding.py
+++ LE_embedding.py
@@ -13,48 +13,45 @@ import numpy as np
 import os.path as osp
 from scipy.spatial.distance import correlation, cosine, euclidean
 from utils.embedding_functions import Laplacain_Eigenmap
-from utils.data_functions import load_task_ROI_TS, compute_SWC, task_labels
-from utils.data_info import DATADIR, PRJDIR, wl_sec, tr, ws_trs, SBJ_list
+from utils.data_functions import compute_SWC
+from utils.data_info import DATADIR, PRJDIR, load_task_ROI_TS, task_labels
 
 def run(args):
     SBJ    = args.subject
+    wl_sec = args.window_length_sec
+    tr     = args.time_resolution
     k      = args.k
     n      = args.n
     metric = args.metric
     print('++INFO: Run information')
     print('        SBJ:   ',SBJ)
+    print('        wl_sec:',wl_sec)
+    print('        tr:    ',tr)
     print('        k:     ',k)
     print('        n:     ',n)
     print('        metric:',metric)
     print(' ')
     
-    # Load ROI time series
-    # --------------------
-    ROI_ts = load_task_ROI_TS(DATADIR,SBJ, wl_sec)
-    print('++INFO: ROI time series loaded')
-    print('        Data shape:',ROI_ts.shape)
-    print(' ')
-    
-    # Compute SWC matrix
-    # ------------------
-    wl_trs = int(wl_sec/tr)
-    window = np.ones((WL_trs,))
-    swc_r, swc_Z, winInfo = compute_swc(pca_df,wl_trs,ws_trs,window=window)
-    SWC_df = swc_Z.reset_index(drop=True).T
-    print('++INFO: SWC matrix computed')
+    # Load SWC matrix
+    # ---------------
+    file_name = SBJ+'_SWC_matrix_wl'+str(wl_sec).zfill(3)+'.csv'
+    file_path = osp.join(PRJADIR,'derivative','SWC',file_name)
+    SWC_df    = pd.read_csv(file_path)  
+    print('++INFO: SWC matrix loaded')
     print('        Data shape:',SWC_df.shape)
     print(' ')
     
     # Drop inbwtween task windows
     # ---------------------------
-    task_df     = task_labels(wl_trs, PURE=False)
+    wl_trs = int(wl_sec/tr)
+    task_df     = task_labels(wl_trs, PURE=False) # USE YOUR OWN FUNCTION TO LOAD TASK LABELS AS PD.DATAFRAME
     drop_index  = task_df.index[task_df['Task'] == 'Inbetween']
     drop_SWC_df = SWC_df.drop(['W'+str(i).zfill(4) for i in drop_index]).reset_index(drop=True)
     print('++INFO: Inbetween task windows dropped')
     print('        Data shape:',drop_SWC_df.shape)
     print(' ')
     
-    # Compure Embedding
+    # Compute Embedding
     # -----------------
     dist_metric_dict = {'correlation':correlation, 'cosine':cosine, 'euclidean':euclidean}
     LE_df = Laplacain_Eigenmap(drop_SWC_df,k=k,n=n,metric=dist_metric_dict[metric])
@@ -64,8 +61,8 @@ def run(args):
     
     # Save file to outside directory
     # ------------------------------
-    out_file = SBJ+'_embedding_data_wl'+str(wl_sec).zfill(3)+'_k'+str(k).zfill(3)+'_n'+str(n).zfill(3)+'_'+metric+'.pkl'
-    out_path = osp.join(PRJADIR,'derivative','Laplacian_Eigenmap',out_file)
+    out_file = SBJ+'_LE_embedding_wl'+str(wl_sec).zfill(3)+'_k'+str(k).zfill(3)+'_n'+str(n).zfill(3)+'_'+metric+'.pkl'
+    out_path = osp.join(PRJADIR,'derivative','LE',out_file)
     LE_data.to_pickle(out_path)
     print('++INFO: Data saved to')
     print('      ',out_path)
@@ -73,6 +70,8 @@ def run(args):
 def main():
     parser=argparse.ArgumentParser(description="Compute embeddings using the Laplacian Eigenmap algorithum.")
     parser.add_argument("-sbj",help="subject name in SBJXX format", dest="subject", type=str, required=True)
+    parser.add_argument("-wl_sec",help="window length in seconds", dest="window_length_sec", type=int, required=True)
+    parser.add_argument("-tr",help="time resolution", dest="time_resolution", type=float, required=True)
     parser.add_argument("-k",help="k-Nearest Neighboor value", dest="k", type=int, required=True)
     parser.add_argument("-n",help="number of dimensions", dest="n", type=int, required=True)
     parser.add_argument("-met",help="distance metric (correlation, cosine, euclidean)", dest="metric", type=str, required=True)
