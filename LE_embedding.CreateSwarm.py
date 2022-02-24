c65b9a7e97e2b227c89709e7e609290603d9fd67
Isabel Fernandez <izyf98@gmail.com>
2022-02-24 10:36:55 -0500

    changed swarm file lines for sh file

--- LE_embedding.CreateSwarm.py
+++ LE_embedding.CreateSwarm.py
@@ -19,7 +19,7 @@
 
 import os
 import os.path as osp
-from utils.data_info import DATADIR, PRJDIR, SBJ_list, wl_sec, tr
+from utils.data_info import PRJDIR, conda_loc, conda_env, SBJ_list, wl_sec, tr
 
 # Create logs directory if doesnt already exist
 os.system('if [ ! -d ../logs ]; then mkdir ../logs; fi')
@@ -35,11 +35,7 @@ os.system('echo "#swarm -f ./LE_embedding.SWARM.sh -g 30 -t 30 --time 8:00:00 --
 for SBJ in SBJ_list:
     for k in [5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100,120,130,140,150,160,170,180,190,200]:
         for metric in ['correlation', 'cosine', 'euclidean']:
-            os.system('echo "./LE_embedding.py -sbj {SBJ} -wl_sec {wl_sec} -tr {tr} -k {k} -n {n} -met {metric}" >> ./LE_embedding.SWARM.sh'.format(SBJ=SBJ,
-                                                                                                                                          wl_sec=wl_sec,
-                                                                                                                                          tr=tr,
-                                                                                                                                          k=k,
-                                                                                                                                          n=n,
-                                                                                                                                          metric=metric))
+            os.system('echo "export PRJDIR={PRJDIR} conda_loc={conda_loc} conda_env={conda_env} SBJ={SBJ} wl_sec={wl_sec} tr={tr} k={k} n={n} metric={metric}; sh ./LE_embedding.sh" >> ./LE_embedding.SWARM.sh'.format(PRJDIR=PRJDIR, conda_loc=conda_loc, conda_env=conda_env, SBJ=SBJ, wl_sec=wl_sec, tr=tr, k=k, n=n, metric=metric))
+
 
 
